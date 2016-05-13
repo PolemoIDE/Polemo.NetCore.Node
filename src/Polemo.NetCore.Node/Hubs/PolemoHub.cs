0fbe7921782b93cc179bc6a404173773d772d101
Jeffiy <zhangmin6105@qq.com>
2016-05-13 22:11:09 +0800

    Init project structure

---
+++ src/Polemo.NetCore.Node/Hubs/PolemoHub.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using Microsoft.Extensions.Logging;
+
+namespace Polemo.NetCore.Node.Hubs
+{
+    [HubName("polemo")]
+    public partial class PolemoHub : Hub
+    {
+        private readonly ILogger _logger;
+
+        public PolemoHub(ILogger<PolemoHub> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task OnConnected()
+        {
+            _logger.LogDebug($"Connected client with ID: {Context?.ConnectionId}");
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            _logger.LogDebug($"Client with ID: {Context?.ConnectionId} Disconnected");
+            return base.OnDisconnected(stopCalled);
+        }
+    }
+}
---
+++ src/Polemo.NetCore.Node/Startup.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Polemo.NetCore.Node
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(c => c.AddPolicy("Polemo", x =>
+                x.AllowCredentials()
+                    .AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+            ));
+
+            services.AddSignalR(options =>
+            {
+                options.Hubs.EnableDetailedErrors = true;
+            });
+
+            services.AddAesCrypto();
+            services.AddSmtpEmailSender("smtp.exmail.qq.com", 25, "码锋科技", "service@codecomb.com", "service@codecomb.com", "Yuuko19931101");
+        }
+
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
+        {
+            app.UseCors("Polemo");
+            app.UseIISPlatformHandler();
+
+            loggerFactory.AddConsole(LogLevel.Debug);
+
+            app.UseIISPlatformHandler();
+//            app.UseFileServer();
+
+            app.UseWebSockets();
+            app.UseSignalR();
+        }
+
+        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+    }
+}
