e89aad75bf5d158327fd910e79a04e3514861716
Peter Wang <wph95@codevs.cn>
2016-05-14 22:39:39 +0800

    complete CreateGitPush

--- src/Polemo.NetCore.Node/Hubs/GitHub.cs
+++ src/Polemo.NetCore.Node/Hubs/GitHub.cs
@@ -293,6 +293,37 @@ namespace Polemo.NetCore.Node.Hubs
             }
             
         }
+        
+        public async Task<object> CreateGitPush(string projectName, string repository, string refspec)
+        {
+            try
+            {
+                var proc = new Process();
+                proc.StartInfo.WorkingDirectory = Path.Combine(Config.RootPath, projectName);
+                proc.StartInfo.FileName = "git";
+                proc.StartInfo.Arguments = "push" + " " + repository +  " " + refspec ;
+                proc.StartInfo.RedirectStandardError = true;
+                proc.StartInfo.RedirectStandardOutput = true;
+                proc.StartInfo.RedirectStandardInput = true;
+                proc.StartInfo.UseShellExecute = false;
+                proc.StartInfo.StandardErrorEncoding = System.Text.Encoding.UTF8;
+                proc.StartInfo.StandardOutputEncoding = System.Text.Encoding.UTF8;
+                proc.Start();
+                while (!proc.WaitForExit(500));
+                var output = proc.StandardOutput.ReadToEnd();
+                var error = proc.StandardError.ReadToEnd();                
+                if (proc.ExitCode != 0)
+                    return new { isSucceeded = false, msg = output + error};
+                return new { isSucceeded = true, msg = output };
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.StackTrace);
+                return new { isSucceeded = false, msg = ex.Message };
+            }
+            
+        }
+        
 
 
     }
