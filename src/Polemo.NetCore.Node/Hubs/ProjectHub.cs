837dea1eedd4bff58e388e7360167f67e6e16b97
Jeffiy <zhangmin6105@qq.com>
2016-05-14 08:58:43 +0800

    complete ReadFile

--- src/Polemo.NetCore.Node/Hubs/ProjectHub.cs
+++ src/Polemo.NetCore.Node/Hubs/ProjectHub.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.SignalR;
+using Microsoft.Extensions.Logging;
 
 namespace Polemo.NetCore.Node.Hubs
 {
@@ -28,9 +30,32 @@ namespace Polemo.NetCore.Node.Hubs
             throw new NotImplementedException();
         }
 
-        public Task<object> ReadFile(string projectName, string fileRelativePath)
+        public async Task<object> ReadFile(string projectName, string fileRelativePath)
         {
-            throw new NotImplementedException();
+            try
+            {
+                string path = Path.Combine(Environment.CurrentDirectory, projectName, fileRelativePath);
+                if (File.Exists(path))
+                {
+                    using (FileStream fileStream = new FileStream(path, FileMode.Open))
+                    {
+                        using (StreamReader reader = new StreamReader(fileStream))
+                        {
+                            var text = await reader.ReadToEndAsync();
+                            return new {IsSucceeded = true, msg = text};
+                        }
+                    }
+                }
+                else
+                {
+                    return new {IsSucceeded = false, msg = "文件不存在"};
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.StackTrace);
+                return new {IsSucceeded = false, msg = ex.Message};
+            }
         }
 
         public Task<object> WriteFile(string projectName, string fileRelativePath, string fileContent)
